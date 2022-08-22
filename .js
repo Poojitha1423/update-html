b8e51f01f0555e3a74bf3e29a584ffe9543954b7
Poojitha <poojitha032320@gmail.com>
2022-08-22 15:07:04 +0530

    update

---
+++ .js
@@ -0,0 +1,15 @@
+const input = document.querySelector('input');
+const log = document.getElementById('log');
+
+input.addEventListener('change', updateValue);
+
+function updateValue(e) {
+  log.textContent = e.target.value;
+}
+
+const selectElement = document.querySelector('.ice-cream');
+
+selectElement.addEventListener('change', (event) => {
+  const result = document.querySelector('.result');
+  result.textContent = `You like ${event.target.value}`;
+});
